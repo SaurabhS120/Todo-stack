f53b3b1d32b87ee761556e4e463eaaa6ca833260
SaurabhS120 <saurabh.sonar120@gmail.com>
2023-11-20 22:11:37 +0530

    log express api calls

--- back_end/express_js/index.js
+++ back_end/express_js/index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const formData = require('express-form-data');
+const winston = require('winston');
 
-// For parsing application/json
-app.use(express.json());
- 
-// For parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(formData.parse());
+
+
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+});
+
+app.use(function(req,res){
+  console.log('Request URL:', req.originalUrl)
+  console.log('Request Type:', req.method)
+  console.log("Request Body : "+JSON.stringify(req.body));
+  console.log("Response Body : "+JSON.stringify(res.body));
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World')
