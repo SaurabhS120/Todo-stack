0d17ddd96dc9799aca06d2cfd850c93a4f978ed0
SaurabhS120 <saurabh.sonar120@gmail.com>
2023-11-20 22:45:01 +0530

    add todo api and html form

--- back_end/express_js/index.js
+++ back_end/express_js/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const formData = require('express-form-data');
+const formData = require('express-form-data');//parse postman post req body
 const winston = require('winston');
+const bodyParser = require('body-parser');//parse html form data
 
 app.use(formData.parse());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 const logger = winston.createLogger({
@@ -15,6 +19,8 @@ app.use(function(req,res,next){
   console.log('Request Type:', req.method)
   console.log("Request Body : "+JSON.stringify(req.body));
   console.log("Response Body : "+JSON.stringify(res.body));
+  console.log("Request Header : "+JSON.stringify(req.header));
+  console.log("Response Header : "+JSON.stringify(res.header));
   next();
 });
 
@@ -23,9 +29,10 @@ app.get('/', function (req, res) {
 })
 
 app.post('/addTodo', function (req, res,next) {
-  console.log(req.body);
-  console.log(res.body);
-  res.send('aDD TODO')
+  console.log("title : "+req.body.title);
+  var responseText = "Todo Added"
+  responseText+="<br> Todo title : "+req.body.title;
+  res.send(responseText);
   next();
 })
 
