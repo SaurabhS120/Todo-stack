1a36b91b322e8aee5435862ee4802c86a6f922c6
SaurabhS120 <saurabh.sonar120@gmail.com>
2023-11-20 22:15:02 +0530

    express freez fix

--- back_end/express_js/index.js
+++ back_end/express_js/index.js
@@ -10,21 +10,23 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 
-app.use(function(req,res){
+app.use(function(req,res,next){
   console.log('Request URL:', req.originalUrl)
   console.log('Request Type:', req.method)
   console.log("Request Body : "+JSON.stringify(req.body));
   console.log("Response Body : "+JSON.stringify(res.body));
+  next();
 });
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.post('/addTodo', function (req, res) {
+app.post('/addTodo', function (req, res,next) {
   console.log(req.body);
   console.log(res.body);
   res.send('aDD TODO')
+  next();
 })
 
 app.listen(3000)
